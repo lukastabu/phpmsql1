debfcdb5600a216b64ac52a926d611078d6894ea
lukastabu <101096807+lukastabu@users.noreply.github.com>
2023-09-29 16:04:36 +0300

    4-1

--- Chapter_04/04_01/globe_bank/public/staff/subjects/edit.php
+++ Chapter_04/04_01/globe_bank/public/staff/subjects/edit.php
@@ -13,7 +13,7 @@ if($test == '404') {
 }
 ?>
 
-<?php $page_title = 'Create Subject'; ?>
+<?php $page_title = 'Edit Subject'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
@@ -21,7 +21,7 @@ if($test == '404') {
   <a class="back-link" href="<?php echo url_for('/staff/subjects/index.php'); ?>">&laquo; Back to List</a>
 
   <div class="subject new">
-    <h1>Create Subject</h1>
+    <h1>Edit Subject</h1>
 
     <form action="" method="post">
       <dl>
@@ -44,7 +44,7 @@ if($test == '404') {
         </dd>
       </dl>
       <div id="operations">
-        <input type="submit" value="Create Subject" />
+        <input type="submit" value="Edit Subject" />
       </div>
     </form>
 
